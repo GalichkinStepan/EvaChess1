29bac77b3ec876b989dc7df78e2633a85aba5000
Степан <s_galichkin@mail.ru>
2024-11-20 18:54:16 +0700

    New project!

---
+++ src/main/java/ru/nstu/EvaChess/EvaChessApplication.java
@@ -0,0 +1,13 @@
+package ru.nstu.EvaChess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaChessApplication.class, args);
+	} // Hello World
+
+}
---
+++ src/test/java/ru/nstu/EvaChess/ShiftLabApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.nstu.EvaChess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
